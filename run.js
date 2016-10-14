d8d57150c5fae827e4cc1e5ce4317f8da1372273
infiniti008 <nikitenok.sl@gmail.com>
2016-10-14 12:00:44 +0300

    Дописал сервер mosca

--- run.js
+++ run.js
@@ -1,7 +1,9 @@
 var request = require('request');
 var express = require('express');
 var fs = require('fs');
+var mosca = require('./mosca.js');
 
 var servers = require('./server.js');
 
-servers.start_server();
+// servers.start_server();
+mosca.srart_mosca();

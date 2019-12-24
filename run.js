fe79d826715b78226e787b10049258e6cc869e45
vyacheslav_nikitenok <vyacheslav.nikitenok@vsgcommerce.com>
2019-12-24 14:40:26 +0300

    Clear repository

--- run.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-var express = require('express');
-var fs = require('fs');
-var mosca = require('./mosca.js');
-
-var servers = require('./server.js');
-
-// servers.start_server();
-mosca.srart_mosca();
